ccc9dd2f2b561f1ab543dd1b65eb8b0c136fb461
Jakob Knicley <knicleyjakob@gmail.com>
2021-04-17 13:38:31 -0400

    changes 2

---
+++ models/Trip.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Trip extends Model {}
+
+Trip.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    trip_budget: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    traveller_amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    traveller_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'trip',
+  }
+);
+
+module.exports = Trip;
